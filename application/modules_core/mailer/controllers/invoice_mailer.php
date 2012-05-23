df1dfe684060a2a69ac001d03191ad1f02c8d04c
Jeroen van Heel <jeroenvheel@gmail.com>
2012-05-23 11:20:22 +0200

    added lex parser to invoice mailer field

--- application/modules_core/mailer/controllers/invoice_mailer.php
+++ application/modules_core/mailer/controllers/invoice_mailer.php
@@ -2,173 +2,123 @@
 
 class Invoice_Mailer extends Admin_Controller {
 
-    function __construct() {
+	function __construct() {
 
-        parent::__construct();
+		parent::__construct();
 
-        $this->load->model(
-            array(
-            'users/mdl_users',
-            'invoices/mdl_invoices',
-            'templates/mdl_templates',
-            'mdl_mailer'
-            )
-        );
+		$this->load->model(
+			array(
+			'users/mdl_users',
+			'invoices/mdl_invoices',
+			'templates/mdl_templates',
+			'mdl_mailer'
+			)
+		);
 
-    }
+	}
 
-    function form() {
+	function form() {
 
-		/**
-		 * Grab the invoice id from url, if exists.
-		 */
-        $invoice_id = uri_assoc('invoice_id', 4);
+		if ($this->input->post('btn_cancel')) {
 
-		/**
-		 * No invoice id in url? Screw it, let's get outta here.
-		 */
-        if (!$invoice_id) {
+			redirect('invoices');
 
-            redirect($this->session->userdata('last_index'));
+		}
 
-        }
-
-		/**
-		 * Was the Cancel button pressed?
-		 */
-        if ($this->input->post('btn_cancel')) {
-
-            redirect('invoices');
-
-        }
-
-		/**
-		 * Load the email template model.
-		 */
 		$this->load->model('email_templates/mdl_email_templates');
 
-		/**
-		 * Set some default values
-		 */
+		$invoice_id = uri_assoc('invoice_id', 4);
+
 		$default_template = $this->mdl_email_templates->get_by_id($this->mdl_mcb_data->setting('default_invoice_email_template'));
 		$email_body = ($default_template) ? $default_template->email_template_body : ' ';
 		$email_footer = ($default_template) ? $default_template->email_template_footer : ' ';
 
-		/**
-		 * Set the parameters for the invoice model.
-		 */
-        $params = array(
-            'where'	=>	array(
-                'mcb_invoices.invoice_id'	=>	$invoice_id
-            ),
-            'get_invoice_items'     =>  TRUE,
-            'get_invoice_tax_rates' =>  TRUE,
-            'get_invoice_payments'  =>  TRUE,
-            'get_invoice_tags'      =>  TRUE
-        );
+		if (!$invoice_id) {
 
-		/**
-		 * Get the invoice object.
-		 */
-        $invoice = $this->mdl_invoices->get($params);
+			redirect($this->session->userdata('last_index'));
 
-		/**
-		 * Does this thing validate yet or what?
-		 */
-        if (!$this->mdl_mailer->validate_invoice_email()) {
+		}
 
-			/**
-			 * If nothing has yet been posted, let's set some form values.
-			 */
-            if (!$_POST) {
+		$params = array(
+			'where'	=>	array(
+				'mcb_invoices.invoice_id'	=>	$invoice_id
+			),
+			'get_invoice_items'     =>  TRUE,
+			'get_invoice_tax_rates' =>  TRUE,
+			'get_invoice_payments'  =>  TRUE,
+			'get_invoice_tags'      =>  TRUE
+		);
 
-                $email_subject = (!$invoice->invoice_is_quote) ? $this->lang->line('invoice') : $this->lang->line('quote');
-                $email_subject .= ' #' . $invoice->invoice_number;
+		$invoice = $this->mdl_invoices->get($params);
 
-                $this->mdl_mailer->set_form_value('email_to', $invoice->client_email_address);
-                $this->mdl_mailer->set_form_value('email_from_name', $invoice->from_first_name . ' ' . $invoice->from_last_name);
-                $this->mdl_mailer->set_form_value('email_from_email', $invoice->from_email_address);
-                $this->mdl_mailer->set_form_value('email_subject', $email_subject);
-                $this->mdl_mailer->set_form_value('email_body', $email_body);
-                $this->mdl_mailer->set_form_value('invoice_template', uri_assoc('invoice_template', 4));
-                $this->mdl_mailer->set_form_value('email_cc', $this->mdl_mcb_data->setting('default_cc'));
-                $this->mdl_mailer->set_form_value('email_bcc', $this->mdl_mcb_data->setting('default_bcc'));
-                $this->mdl_mailer->set_form_value('email_footer', $email_footer);
+		if (!$this->mdl_mailer->validate_invoice_email()) {
 
-            }
+			if (!$_POST) {
 
-			/**
-			 * Prepare the data for the view.
-			 */
-            $data = array(
-                'templates'	=>	$this->mdl_templates->get('invoices')
-            );
+				$email_subject = (!$invoice->invoice_is_quote) ? $this->lang->line('invoice') : $this->lang->line('quote');
+				$email_subject .= ' #' . $invoice->invoice_number;
 
-			/**
-			 * Duh, load the view.
-			 */
-            $this->load->view('invoice_mailer', $data);
+				$this->mdl_mailer->set_form_value('email_to', $invoice->client_email_address);
+				$this->mdl_mailer->set_form_value('email_from_name', $invoice->from_first_name . ' ' . $invoice->from_last_name);
+				$this->mdl_mailer->set_form_value('email_from_email', $invoice->from_email_address);
+				$this->mdl_mailer->set_form_value('email_subject', $email_subject);
+				$this->mdl_mailer->set_form_value('email_body', $email_body);
+				$this->mdl_mailer->set_form_value('invoice_template', uri_assoc('invoice_template', 4));
+				$this->mdl_mailer->set_form_value('email_cc', $this->mdl_mcb_data->setting('default_cc'));
+				$this->mdl_mailer->set_form_value('email_bcc', $this->mdl_mcb_data->setting('default_bcc'));
+				$this->mdl_mailer->set_form_value('email_footer', $email_footer);
 
-        }
+			}
 
-        else {
+			$data = array(
+				'templates'	=>	$this->mdl_templates->get('invoices'),
+				'parse'		=>	$invoice
+			);
 
-			/**
-			 * Finally, the freakin' form has validated.
-			 * Now let's quit jerking around and load a few things.
-			 */
-			$this->load->library('invoices/lib_output');
+			$this->parser->parse('invoice_mailer', $data);
+			//$this->load->view('invoice_mailer', $data);
+			
 
-			/**
-			 * Here's a bunch of stuff we'll cram into the email.
-			 */
-            $invoice_template = $this->input->post('invoice_template');
-            $from_email = $this->input->post('email_from_email');
-            $from_name = $this->input->post('email_from_name');
-            $to = $this->input->post('email_to');
-            $subject = $this->input->post('email_subject');
-            $email_body = $this->input->post('email_body');
-            $email_cc = $this->input->post('email_cc');
-            $email_bcc = $this->input->post('email_bcc');
-            $invoice_as_body = $this->input->post('invoice_as_body');
-            $email_footer = $this->input->post('email_footer');
+		}
 
-			/**
-			 * Shoot the email out into space in hopes a civilization far, far
-			 * way shall receive it, decipher it, locate our planet and destroy
-			 * us all.
-			 */
-            $this->mdl_mailer->email_invoice(
-                $invoice,
-                $invoice_template,
-                $from_email,
-                $from_name,
-                $to,
-                $subject,
-                $email_body,
-                $email_footer,
-                $invoice_as_body,
-                $email_cc,
-                $email_bcc
-            );
+		else {
 
-			$this->load->model('invoices/mdl_invoice_history');
-			$this->mdl_invoice_history->save($invoice->invoice_id, $this->session->userdata('user_id'), $this->lang->line('emailed_invoice') . ' to ' . $to);
+			$invoice_template = $this->input->post('invoice_template');
+			$from_email = $this->input->post('email_from_email');
+			$from_name = $this->input->post('email_from_name');
+			$to = $this->input->post('email_to');
+			$subject = $this->input->post('email_subject');
+			$email_body = $this->input->post('email_body');
+			$email_cc = $this->input->post('email_cc');
+			$email_bcc = $this->input->post('email_bcc');
+			$invoice_as_body = $this->input->post('invoice_as_body');
+			$email_footer = $this->input->post('email_footer');
 
-			/**
-			 * Ok, we're done here. Let's move on.
-			 */
-            redirect($this->session->userdata('last_index'));
+			$this->mdl_mailer->email_invoice(
+				$invoice,
+				$invoice_template,
+				$from_email,
+				$from_name,
+				$to,
+				$subject,
+				$email_body,
+				$email_footer,
+				$invoice_as_body,
+				$email_cc,
+				$email_bcc
+			);
 
-        }
+			redirect($this->session->userdata('last_index'));
 
-    }
+		}
 
-    function overdue() {
+	}
 
-        if (!$this->mdl_mailer->validate_invoice_overdue()) {
+	function overdue() {
 
-            if (!$_POST) {
+		if (!$this->mdl_mailer->validate_invoice_overdue()) {
+
+			if (!$_POST) {
 
 				$this->load->model('email_templates/mdl_email_templates');
 
@@ -176,81 +126,82 @@ class Invoice_Mailer extends Admin_Controller {
 				$email_body = ($default_template) ? $default_template->email_template_body : ' ';
 				$email_footer = ($default_template) ? $default_template->email_template_footer : ' ';
 
-                $user = $this->mdl_users->get_by_id($this->session->userdata('user_id'));
+				$user = $this->mdl_users->get_by_id($this->session->userdata('user_id'));
 
-                $this->mdl_mailer->set_form_value('email_from_name', $user->first_name . ' ' . $user->last_name);
-                $this->mdl_mailer->set_form_value('email_from_email', $user->email_address);
-                $this->mdl_mailer->set_form_value('email_subject', $this->lang->line('overdue_invoice_reminder'));
-                $this->mdl_mailer->set_form_value('email_body', $email_body);
-                $this->mdl_mailer->set_form_value('email_footer', $email_footer);
+				$this->mdl_mailer->set_form_value('email_from_name', $user->first_name . ' ' . $user->last_name);
+				$this->mdl_mailer->set_form_value('email_from_email', $user->email_address);
+				$this->mdl_mailer->set_form_value('email_subject', $this->lang->line('overdue_invoice_reminder'));
+				$this->mdl_mailer->set_form_value('email_body', $email_body);
+				$this->mdl_mailer->set_form_value('email_footer', $email_footer);
 
-                $this->mdl_mailer->set_form_value('invoice_template', $this->mdl_mcb_data->setting('default_invoice_template'));
+				$this->mdl_mailer->set_form_value('invoice_template', $this->mdl_mcb_data->setting('default_invoice_template'));
 
-            }
+			}
 
-            $data = array(
-                'invoices'  =>  $this->mdl_invoices->get_overdue(),
-                'templates' =>  $this->mdl_templates->get('invoices')
-            );
+			$data = array(
+				'invoices'  =>  $this->mdl_invoices->get_overdue(),
+				'templates' =>  $this->mdl_templates->get('invoices')
+			);
 
-            $this->load->view('invoice_overdue', $data);
+			$this->load->view('invoice_overdue', $data);
 
-        }
+		}
 
-        else {
+		else {
 
-            if ($this->input->post('invoice_ids')) {
+			if ($this->input->post('invoice_ids')) {
 
-                $invoice_template = $this->input->post('invoice_template');
-                $from_email = $this->input->post('email_from_email');
-                $from_name = $this->input->post('email_from_name');
-                $subject = $this->input->post('email_subject');
-                $email_body = $this->input->post('email_body');
-                $email_cc = $this->input->post('email_cc');
-                $email_bcc = $this->input->post('email_bcc');
-                $invoice_as_body = $this->input->post('invoice_as_body');
-                $email_addresses = $this->input->post('email_address');
-                $email_footer = $this->input->post('email_footer');
+				$invoice_template = $this->input->post('invoice_template');
+				$from_email = $this->input->post('email_from_email');
+				$from_name = $this->input->post('email_from_name');
+				$subject = $this->input->post('email_subject');
+				$email_body = $this->input->post('email_body');
+				$email_cc = $this->input->post('email_cc');
+				$email_bcc = $this->input->post('email_bcc');
+				$invoice_as_body = $this->input->post('invoice_as_body');
+				$email_addresses = $this->input->post('email_address');
+				$email_footer = $this->input->post('email_footer');
+				
+				foreach ($this->input->post('invoice_ids') as $invoice_id) {
 
-                foreach ($this->input->post('invoice_ids') as $invoice_id) {
+					$params = array(
+						'where' =>  array(
+							'mcb_invoices.invoice_id'   =>  $invoice_id
+						),
+						'get_invoice_items'     =>  TRUE,
+						'get_invoice_tax_rates' =>  TRUE,
+						'get_invoice_payments'  =>  TRUE,
+						'get_invoice_tags'      =>  TRUE
+					);
 
-                    $params = array(
-                        'where' =>  array(
-                            'mcb_invoices.invoice_id'   =>  $invoice_id
-                        ),
-                        'get_invoice_items'     =>  TRUE,
-                        'get_invoice_tax_rates' =>  TRUE,
-                        'get_invoice_payments'  =>  TRUE,
-                        'get_invoice_tags'      =>  TRUE
-                    );
+					$invoice = $this->mdl_invoices->get($params);
+					
+					$to = $email_addresses[$invoice_id];
 
-                    $invoice = $this->mdl_invoices->get($params);
+					$this->mdl_mailer->email_invoice(
+						$invoice,
+						$invoice_template,
+						$from_email,
+						$from_name,
+						$to,
+						$subject,
+						$email_body,
+						$email_footer,
+						$invoice_as_body,
+						$email_cc,
+						$email_bcc
+					);
 
-                    $to = $email_addresses[$invoice_id];
 
-                    $this->mdl_mailer->email_invoice(
-                        $invoice,
-                        $invoice_template,
-                        $from_email,
-                        $from_name,
-                        $to,
-                        $subject,
-                        $email_body,
-                        $email_footer,
-                        $invoice_as_body,
-                        $email_cc,
-                        $email_bcc
-                    );
+				}
 
-                }
+			}
+			
+			redirect($this->session->userdata('last_index'));
 
-            }
+		}
 
-            redirect($this->session->userdata('last_index'));
-
-        }
-
-    }
+	}
 
 }
 
